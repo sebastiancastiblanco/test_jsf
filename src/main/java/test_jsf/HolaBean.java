12f0d03ab5d4c24b74803a807e908563245dca74
scastiblanco <scastiblanco@acsendo.com>
2019-02-12 16:43:10 -0500

    init commit

---
+++ src/main/java/test_jsf/HolaBean.java
@@ -0,0 +1,28 @@
+package test_jsf;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="holaBean")
+@SessionScoped
+public class HolaBean implements Serializable {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String nombre;
+
+    public void  HolaMundo() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }     
+}
